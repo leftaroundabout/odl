8de0b7bebf37481252198fe87ad818ef9c2b20af
Justus Sagemüller <justussa@kth.se>
2025-03-06 11:33:56 +0100

    Use `moveaxis` instead of the semantically more obscure `rollaxis`.
    This change was suggested by Jevgenija in https://github.com/odlgroup/odl/pull/1634#pullrequestreview-1877765438.
    
    The NumPy documentation also recommends `moveaxis` over `rollaxis`.

--- odl/tomo/backends/astra_cuda.py
+++ odl/tomo/backends/astra_cuda.py
@@ -236,13 +236,13 @@ class AstraCudaImpl:
             if self.geometry.ndim == 2:
                 out[:] = self.proj_array
             elif self.geometry.ndim == 3:
-                # TODO: Find a way not to have to do rollaxis(0, 3) for
+                # TODO: Find a way not to have to do moveaxis(0, 2) for
                 # cylindrical detectors (probably inside ASTRA)
                 if getattr(self.geometry, 'det_curvature_radius', None) is None:
                     out[:] = np.swapaxes(self.proj_array, 0, 1).reshape(
                         self.proj_space.shape)
                 else:
-                    out[:] = np.rollaxis(self.proj_array, 0, 3).reshape(
+                    out[:] = np.moveaxis(self.proj_array, 0, 2).reshape(
                         self.proj_space.shape)
 
             # Fix scaling to weight by pixel size
@@ -292,7 +292,7 @@ class AstraCudaImpl:
             elif self.geometry.ndim == 3:
                 shape = (-1,) + self.geometry.det_partition.shape
                 reshaped_proj_data = proj_data.asarray().reshape(shape)
-                # TODO: Find a way not to have to do rollaxis(2, 0) for
+                # TODO: Find a way not to have to do moveaxis(2, 0) for
                 # cylindrical detectors (probably inside ASTRA)
                 if getattr(self.geometry, 'det_curvature_radius', None) is None:
                     swapped_proj_data = np.ascontiguousarray(
@@ -300,7 +300,7 @@ class AstraCudaImpl:
                     )
                 else:
                     swapped_proj_data = np.ascontiguousarray(
-                        np.rollaxis(reshaped_proj_data, 2, 0)
+                        np.moveaxis(reshaped_proj_data, 2, 0)
                     )
                 astra.data3d.store(self.sino_id, swapped_proj_data)
 
