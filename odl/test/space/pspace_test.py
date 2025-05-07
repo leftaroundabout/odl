2c5ff88270715ec7e35f565c09a7b881a952f38f
emilien <emilienvalat@gmail.com>
2025-05-07 14:19:34 +0200

    Change of the testutil 'noise_array' function for handling product spaces. This is due to the fact that the test must accomodate for different backends. Before, the noise array for product spaces was wrapped in a numpy array directly in testutils. Now, it is wrapped explicitely in the test file for numpy implementation

--- odl/test/space/pspace_test.py
+++ odl/test/space/pspace_test.py
@@ -844,7 +844,8 @@ def test_unary_ops():
 
     for op in [operator.pos, operator.neg]:
         x_arr, x = noise_elements(pspace)
-
+        # Here, we explictely wrap inside a numpy array as we removed the wrapping for power spaces from the testutils file
+        x_arr = np.array(x_arr)
         y_arr = op(x_arr)
         y = op(x)
 
@@ -864,6 +865,8 @@ def test_operators(odl_arithmetic_op):
 
         # Left op
         x_arr, x = noise_elements(pspace)
+        # Here, we explictely wrap inside a numpy array as we removed the wrapping for power spaces from the testutils file
+        x_arr = np.array(x_arr)
         if scalar == 0 and op in [operator.truediv, operator.itruediv]:
             # Check for correct zero division behaviour
             with pytest.raises(ZeroDivisionError):
@@ -876,7 +879,8 @@ def test_operators(odl_arithmetic_op):
 
         # Right op
         x_arr, x = noise_elements(pspace)
-
+        # Here, we explictely wrap inside a numpy array as we removed the wrapping for power spaces from the testutils file
+        x_arr = np.array(x_arr)
         y_arr = op(scalar, x_arr)
         y = op(scalar, x)
 
@@ -885,7 +889,9 @@ def test_operators(odl_arithmetic_op):
     # Verify that the statement z=op(x, y) gives equivalent results to NumPy
     x_arr, x = noise_elements(space, 1)
     y_arr, y = noise_elements(pspace, 1)
-
+    # Here, we explictely wrap inside a numpy array as we removed the wrapping for power spaces from the testutils file
+    x_arr = np.array(x_arr)
+    y_arr = np.array(y_arr)
     # non-aliased left
     if op in [operator.iadd, operator.isub, operator.itruediv, operator.imul]:
         # Check for correct error since in-place op is not possible here
@@ -965,7 +971,7 @@ def test_np_reductions():
 
 def test_array_wrap_method():
     """Verify that the __array_wrap__ method for NumPy works."""
-    space = odl.ProductSpace(odl.uniform_discr(0, 1, 10), 2)
+    space = odl.ProductSpace(odl.uniform_discr(0, 1, 10, device='cpu'), 2)
     x_arr, x = noise_elements(space)
     y_arr = np.sin(x_arr)
     y = np.sin(x)  # Should yield again an ODL product space element
